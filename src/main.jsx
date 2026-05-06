f818307fcf0e7c377169d9f7a0c8e83ffa01b197
Nabila Yuliana <nabila.123140099@student.itera.ac.id>
2026-05-06 09:29:40 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import "leaflet/dist/leaflet.css"; // ⚠️ WAJIB
+import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
